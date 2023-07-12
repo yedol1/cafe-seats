181a7a6ae14b2675c9df68f19ecc41c2adb1158a
yedol1 <yedol1@naver.com>
2023-07-12 11:48:48 +0900

    add: 리덕스 툴킷사용하여 그림판 그리기

--- src/App.js
+++ src/App.js
@@ -1,8 +1,78 @@
-function App() {
+import React, { useRef, useEffect } from "react";
+import { useSelector, useDispatch, Provider } from "react-redux";
+import { createSlice, configureStore } from "@reduxjs/toolkit";
+
+const drawingBoardSlice = createSlice({
+  name: "drawingBoard",
+  initialState: { color: "#000000" },
+  reducers: {
+    setColor: (state, action) => {
+      state.color = action.payload;
+    },
+  },
+});
+
+const { setColor } = drawingBoardSlice.actions;
+
+const store = configureStore({ reducer: drawingBoardSlice.reducer });
+
+function DrawingBoard() {
+  const color = useSelector((state) => state.color);
+  const dispatch = useDispatch();
+  const canvasRef = useRef(null);
+  let painting = false;
+
+  useEffect(() => {
+    const canvas = canvasRef.current;
+    const ctx = canvas.getContext("2d");
+
+    const startPosition = (e) => {
+      painting = true;
+      draw(e);
+    };
+
+    const finishedPosition = () => {
+      painting = false;
+      ctx.beginPath();
+    };
+
+    const draw = (e) => {
+      if (!painting) return;
+      ctx.lineWidth = 10;
+      ctx.lineCap = "round";
+      ctx.strokeStyle = color;
+
+      ctx.lineTo(e.clientX, e.clientY);
+      ctx.stroke();
+      ctx.beginPath();
+      ctx.moveTo(e.clientX, e.clientY);
+    };
+
+    canvas.addEventListener("mousedown", startPosition);
+    canvas.addEventListener("mouseup", finishedPosition);
+    canvas.addEventListener("mousemove", draw);
+
+    return () => {
+      canvas.removeEventListener("mousedown", startPosition);
+      canvas.removeEventListener("mouseup", finishedPosition);
+      canvas.removeEventListener("mousemove", draw);
+    };
+  }, [color]);
+
   return (
     <div>
-      hello world
+      <canvas ref={canvasRef} width="500" height="500" style={{ border: "1px solid #000000" }} />
+      <input type="color" value={color} onChange={(e) => dispatch(setColor(e.target.value))} />
     </div>
   );
 }
+
+function App() {
+  return (
+    <Provider store={store}>
+      <DrawingBoard />
+    </Provider>
+  );
+}
+
 export default App;
